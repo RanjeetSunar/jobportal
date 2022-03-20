e1bd62434a0fa4eabaae193dc8f032237731ecb3
RanjeetSunar <predatorapex15@gmail.com>
2022-03-20 17:43:47 +0545

    second Commit

--- frontend/src/Header/header.js
+++ frontend/src/Header/header.js
@@ -23,7 +23,7 @@ class Header extends Component{
 					<span class="icon-bar"></span>
 				</button>
 						<a class="navbar-brand" href="index.html">
-							<h1><span class="fa fa-signal" aria-hidden="true"></span> Soft <label>Hr Agency</label></h1>
+							<h1><span class="fa fa-signal" aria-hidden="true"></span> Just <label>Work</label></h1>
 						</a>
 					</div>
 					{/* <!--/.navbar-header--> */}
